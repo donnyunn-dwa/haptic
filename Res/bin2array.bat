b435ca65f8d0d4b0726859855363729b0b2463db
donnyunn <donnyunn@dwanatech.com>
2019-11-22 15:55:39 +0900

    reset

--- Res/bin2array.bat
+++
@@ -1,4 +0,0 @@
-cd ..\Res 
-srec_cat.exe sine130.bin -Binary -o freqLow.c -C-Array freqLow -INClude
-srec_cat.exe sine165.bin -Binary -o freqMid.c -C-Array freqMid -INClude
-srec_cat.exe sine200.bin -Binary -o freqHigh.c -C-Array freqHigh -INClude
